24057ba08771d0d765b83640cab4140844b48f16
dvghub <dieuwer.greevenbroek@gmail.com>
2021-03-19 00:28:23 +0100

    rewritten for serialiser use

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,11 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Extra;
-use App\Entity\Ingredient;
 use App\Entity\Recipe;
-use App\Entity\Step;
-use App\Entity\Tag;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,9 +12,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipeController extends AbstractController {
-    public function create(Request $request, EntityManagerInterface $manager): Response {
-        $data = $request->toArray();
-        $recipe = $this->fillRecipe(new Recipe(), $data);
+    public function create(
+        Request $request,
+        EntityManagerInterface $manager,
+        SerializerInterface $serializer
+    ): Response {
+        $recipe = $serializer->deserialize(
+            $request->getContent(), Recipe::class, 'json', [ 'groups' => 'recipe' ]
+        );
 
         foreach($recipe->getIngredients() as $ingredient) {
             $manager->persist($ingredient);
@@ -43,49 +44,59 @@ class RecipeController extends AbstractController {
     }
 
     public function all(RecipeRepository $repository, SerializerInterface $serializer): Response {
-        return new Response($serializer->serialize($repository->findAll(), 'json', ['groups' => 'recipe']));
+        return new Response($serializer->serialize($repository->findAll(), 'json', [ 'groups' => 'recipe' ]));
     }
 
-    public function read(int $id, RecipeRepository $repository): Response {
+    public function read(int $id, RecipeRepository $repository, SerializerInterface $serializer): Response {
         $recipe = $repository->find($id);
 
-        return new Response(json_encode($recipe->toArray()));
+        return new Response($serializer->serialize($recipe, 'json', [ 'groups' => 'recipe' ]));
     }
 
     public function update(
         int $id,
         Request $request,
         EntityManagerInterface $manager,
-        RecipeRepository $repository
+        RecipeRepository $repository,
+        SerializerInterface $serializer
     ): Response {
-        $data = $request->toArray();
         $recipe = $repository->find($id);
-        $updatedRecipe = $this->fillRecipe(new Recipe(), $data);
+        $updatedRecipe = $serializer->deserialize(
+            $request->getContent(), Recipe::class, 'json', [ 'groups' => 'recipe' ]
+        );
 
         $recipe->setName($updatedRecipe->getName());
         $recipe->setPeople($updatedRecipe->getPeople());
         $recipe->setPreparationTime($updatedRecipe->getPreparationTime());
         $recipe->setWaitTime($updatedRecipe->getWaitTime());
 
-        foreach($recipe->getIngredients() as $ingredient) { $recipe->removeIngredient($ingredient); }
+        foreach($recipe->getIngredients() as $ingredient) {
+            $recipe->removeIngredient($ingredient);
+        }
         foreach($updatedRecipe->getIngredients() as $ingredient) {
             $manager->persist($ingredient);
             $recipe->addIngredient($ingredient);
         }
 
-        foreach($recipe->getSteps() as $step) { $recipe->removeStep($step); }
+        foreach($recipe->getSteps() as $step) {
+            $recipe->removeStep($step);
+        }
         foreach($updatedRecipe->getSteps() as $step) {
             $manager->persist($step);
             $recipe->addStep($step);
         }
 
-        foreach($recipe->getExtras() as $extra) { $recipe->removeExtra($extra); }
+        foreach($recipe->getExtras() as $extra) {
+            $recipe->removeExtra($extra);
+        }
         foreach($updatedRecipe->getExtras() as $extra) {
             $manager->persist($extra);
             $recipe->addExtra($extra);
         }
 
-        foreach($recipe->getTags() as $tag) { $recipe->removeTag($tag); }
+        foreach($recipe->getTags() as $tag) {
+            $recipe->removeTag($tag);
+        }
         foreach($updatedRecipe->getTags() as $tag) {
             $manager->persist($tag);
             $recipe->addTag($tag);
@@ -94,7 +105,7 @@ class RecipeController extends AbstractController {
         $manager->persist($recipe);
         $manager->flush();
 
-        return new Response(json_encode($recipe->toArray()));
+        return new Response($serializer->serialize($recipe, 'json', ['groups' => 'recipe']));
     }
 
     public function delete(int $id, EntityManagerInterface $manager, RecipeRepository $repository): Response {
@@ -103,45 +114,5 @@ class RecipeController extends AbstractController {
         $manager->flush();
 
         return new Response('Deleted recipe "' . $recipe->getName() . '"!');
-    }
-
-    private function fillRecipe(Recipe $recipe, array $data): Recipe {
-        $recipe->setName($data['name']);
-        $recipe->setPeople($data['people']);
-        $recipe->setPreparationTime($data['preparation_time']);
-        $recipe->setWaitTime($data['wait_time']);
-
-        foreach($data['ingredients'] as $ingredientData) {
-            $ingredient = new Ingredient();
-            $ingredient->setAmount($ingredientData['amount']);
-            $ingredient->setQuantity($ingredientData['quantity']);
-            $ingredient->setName($ingredientData['name']);
-
-            $recipe->addIngredient($ingredient);
-        }
-
-        foreach($data['steps'] as $stepData) {
-            $step = new Step();
-            $step->setIndex($stepData['index']);
-            $step->setText($stepData['text']);
-
-            $recipe->addStep($step);
-        }
-
-        foreach($data['extras'] as $extraData) {
-            $extra = new Extra();
-            $extra->setText($extraData['text']);
-
-            $recipe->addExtra($extra);
-        }
-
-        foreach($data['tags'] as $tagData) {
-            $tag = new Tag();
-            $tag->setName($tagData['name']);
-
-            $recipe->addTag($tag);
-        }
-
-        return $recipe;
     }
 }
